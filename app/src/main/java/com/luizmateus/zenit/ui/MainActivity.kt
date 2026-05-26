89fe00e0426133bf784488aa4aef0be780be705f
luizgmo <luiz18212returns@gmail.com>
2026-05-26 10:26:47 -0300

    feat: tela inicial

--- app/src/main/java/com/luizmateus/zenit/ui/MainActivity.kt
+++ app/src/main/java/com/luizmateus/zenit/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.luizmateus.zenit
+package com.luizmateus.zenit.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.luizmateus.zenit.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
